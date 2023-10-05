22df1765d30c3908c837bbdbb5041098089b2980
Appiah Ernest <appiahernest541@gmail.com>
2023-10-05 11:47:12 +0000

    first-component-counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState }  from 'react';
+import "./Components/s.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [number, setNumber] = useState (0);
+  const [age, setAge] = useState (23);
+
+  const handleIncrease = () => {
+    setAge(age+2);
+    setNumber(number+1);
+  }
+
+  const handleDecrease = () => {
+    setAge(age-2);
+    setNumber(number-1);
+  }
+
+
+  return(
+  <div className='App'> 
+  <h1>{age}</h1>
+  <h1>{number}</h1>
+  <button onClick={handleIncrease}>Increase</button>
+  <button onClick={handleDecrease}>Decrease</button>
+  
+
+  </div>)
 }
 
 export default App;
